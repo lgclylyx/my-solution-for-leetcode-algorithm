8e1a48333b5ce4733083092d4616490d7b337bea
lgclylyx <lgclylyx@outlook.com>
2017-07-06 12:06:30 +0800

    3sum m

--- 3Sum.cc
+++ 3Sum.cc
@@ -62,3 +62,54 @@ A solution set is:
         return result;
     }
 */
+
+/*
+
+	先将数组排序，则可以对已排序的数组依次处理，通过对数组的一次遍历，可以找到处理位置相匹配的所有数字对，只需O(n^2)
+	
+ 	vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        if(nums.size() <= 2)
+            return result;
+        
+        sort(nums.begin(),nums.end());
+        
+        for(int i = 0; i < nums.size(); i++){
+            //顺序数组的twoSum的策略（twosum:找到两个数字的和为target）
+            int target = -nums[i];
+            int front = i + 1;
+            int back = nums.size() - 1;
+
+            while (front < back) {
+
+                int sum = nums[front] + nums[back];
+            
+                if (sum < target)
+                    front++;
+
+                else if (sum > target)
+                    back--;
+
+                else {
+                    vector<int> triplet(3, 0);
+                    triplet[0] = nums[i];
+                    triplet[1] = nums[front];
+                    triplet[2] = nums[back];
+                    result.push_back(triplet);
+                
+                    while (front < back && nums[front] == triplet[1]) front++;//跳过重复情况
+
+                    while (front < back && nums[back] == triplet[2]) back--;//跳过重复情况
+                }
+            
+            }
+
+            while (i + 1 < nums.size() && nums[i + 1] == nums[i]) //跳过重复情况
+                i++;
+
+        }
+        
+        return result;
+    }
+*/
+
